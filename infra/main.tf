153e6c05ac021e9424e13b62dfac780dbe7c0c6b
richardkyk <28803078+richardkyk@users.noreply.github.com>
2025-08-18 23:06:59 +1000

    refactor: use dynamodb

--- infra/main.tf
+++ infra/main.tf
@@ -9,7 +9,7 @@ terraform {
 }
 
 locals {
-  image_tag = "v1"
+  image_tag = "v3"
 }
 
 # Create ECR repository to hold your Docker image
@@ -68,6 +68,99 @@ resource "aws_lambda_function" "lambda" {
   timeout       = 10
   memory_size   = 512
 
+  environment {
+    variables = {
+      DYNAMODB_TABLE = aws_dynamodb_table.dynamodb_table.name
+    }
+  }
 }
 
+resource "aws_dynamodb_table" "dynamodb_table" {
+  name         = "${var.app_name}-table"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+}
+
+resource "aws_iam_policy" "lambda_dynamodb" {
+  name        = "${var.app_name}-lambda-dynamodb-policy"
+  description = "Allow Lambda to read/write to DynamoDB table"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = [
+          "dynamodb:GetItem",
+          "dynamodb:Query",
+          "dynamodb:Scan",
+          "dynamodb:PutItem",
+          "dynamodb:UpdateItem"
+        ],
+        Effect   = "Allow",
+        Resource = aws_dynamodb_table.dynamodb_table.arn
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = aws_iam_policy.lambda_dynamodb.arn
+}
+
+resource "aws_api_gateway_rest_api" "api" {
+  name        = "${var.app_name}-rest-api"
+  description = "REST API for Lambda proxy integration"
+}
+
+resource "aws_api_gateway_resource" "api_proxy" {
+  rest_api_id = aws_api_gateway_rest_api.api.id
+  parent_id   = aws_api_gateway_rest_api.api.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "any_method" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  resource_id   = aws_api_gateway_resource.api_proxy.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_proxy" {
+  rest_api_id             = aws_api_gateway_rest_api.api.id
+  resource_id             = aws_api_gateway_resource.api_proxy.id
+  http_method             = aws_api_gateway_method.any_method.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.lambda.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_api_gateway_rest_api.api.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_stage" "prod" {
+  rest_api_id   = aws_api_gateway_rest_api.api.id
+  stage_name    = "prod"
+  deployment_id = aws_api_gateway_deployment.deployment.id
+}
+
+resource "aws_api_gateway_deployment" "deployment" {
+  depends_on  = [aws_api_gateway_integration.lambda_proxy]
+  rest_api_id = aws_api_gateway_rest_api.api.id
+}
+
+
+output "api_url" {
+  value = "https://${aws_api_gateway_rest_api.api.id}.execute-api.${var.aws_region}.amazonaws.com/prod"
+}
 
--- main.go
+++ main.go
@@ -2,38 +2,108 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 	"regexp"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/gocolly/colly/v2"
+	"github.com/google/uuid"
 )
 
-type QueryParams struct {
-	Url      string `json:"url"`
-	Selector string `json:"selector"`
-	Regex    string `json:"regex"`
+var (
+	dynamoDBClient *dynamodb.Client
+)
+
+func init() {
+	// Initialize the DynamoDB client outside of the handler, during the init phase
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatalf("unable to load SDK config, %v", err)
+	}
+	dynamoDBClient = dynamodb.NewFromConfig(cfg)
 }
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	params, err := parseQueryParams(request.QueryStringParameters)
+	response := ""
+	var err error
+	fmt.Println(request.HTTPMethod)
+	switch request.HTTPMethod {
+	case "GET":
+		response, err = get(ctx, request)
+	case "POST":
+		response, err = post(ctx, request)
+	default:
+		err = fmt.Errorf("Invalid method")
+	}
 
 	if err != nil {
-		log.Printf("Failed to parse input: %v", err)
 		return events.APIGatewayProxyResponse{
 			StatusCode: 400,
 			Body:       err.Error(),
 		}, nil
 	}
 
+	return events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		Headers:         map[string]string{"Content-Type": "text/plain; charset=utf-8"},
+		Body:            response,
+		IsBase64Encoded: false,
+	}, nil
+
+}
+
+func main() {
+	lambda.Start(handler)
+}
+
+type Event struct {
+	ID string `json:"id"`
+}
+
+func get(ctx context.Context, request events.APIGatewayProxyRequest) (string, error) {
+	tableName := os.Getenv("DYNAMODB_TABLE")
+	id := request.QueryStringParameters["id"]
+	if id == "" {
+		return "", fmt.Errorf("id is required")
+	}
+
+	// Get item from DynamoDB
+	out, err := dynamoDBClient.GetItem(ctx, &dynamodb.GetItemInput{
+		TableName: aws.String(tableName),
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: id},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+
+	// Convert DynamoDB item to simple map[string]string
+	item := make(map[string]string)
+	for k, v := range out.Item {
+		if s, ok := v.(*types.AttributeValueMemberS); ok {
+			item[k] = s.Value
+		}
+	}
+
 	c := colly.NewCollector()
 
-	price := ""
-	priceRegex := regexp.MustCompile(params.Regex)
+	url := item["url"]
+	selector := item["selector"]
+	regex := item["regex"]
 
-	c.OnHTML(params.Selector, func(e *colly.HTMLElement) {
+	price := ""
+	priceRegex := regexp.MustCompile(regex)
+
+	c.OnHTML(selector, func(e *colly.HTMLElement) {
 		text := e.Text
 		match := priceRegex.FindStringSubmatch(text)
 		price = "N/A"
@@ -51,40 +121,55 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		price = fmt.Sprintf("Error: %s", err.Error())
 	})
 
-	c.Visit(params.Url)
+	c.Visit(url)
 	log.Printf("price: %s", price)
+	return price, nil
 
-	return events.APIGatewayProxyResponse{
-		StatusCode:      200,
-		Headers:         map[string]string{"Content-Type": "text/plain; charset=utf-8"},
-		Body:            price,
-		IsBase64Encoded: false,
-	}, nil
 }
 
-func main() {
-	lambda.Start(handler)
+type RequestBody struct {
+	URL      string `json:"url"`
+	Selector string `json:"selector"`
+	Regex    string `json:"regex"`
 }
 
-func parseQueryParams(q map[string]string) (QueryParams, error) {
-	url := q["url"]
-	if url == "" {
-		return QueryParams{}, fmt.Errorf("missing param: url")
+func post(ctx context.Context, request events.APIGatewayProxyRequest) (string, error) {
+	// Parse POST body
+	var body RequestBody
+	err := json.Unmarshal([]byte(request.Body), &body)
+	if err != nil {
+		return "", fmt.Errorf("failed to parse body: %w", err)
+	}
+	if body.URL == "" {
+		return "", fmt.Errorf("url is required")
 	}
 
-	selector := q["selector"]
-	if selector == "" {
-		return QueryParams{}, fmt.Errorf("missing param: selector")
+	if body.Selector == "" {
+		return "", fmt.Errorf("selector is required")
 	}
 
-	regex := q["regex"]
-	if regex == "" {
-		return QueryParams{}, fmt.Errorf("missing param: regex")
+	if body.Regex == "" {
+		return "", fmt.Errorf("regex is required")
 	}
 
-	return QueryParams{
-		Url:      url,
-		Selector: selector,
-		Regex:    regex,
-	}, nil
+	// Generate a UUID for the item
+	id := uuid.New().String()
+
+	tableName := os.Getenv("DYNAMODB_TABLE")
+	// Put item into DynamoDB
+	_, err = dynamoDBClient.PutItem(ctx, &dynamodb.PutItemInput{
+		TableName: aws.String(tableName),
+		Item: map[string]types.AttributeValue{
+			"id":       &types.AttributeValueMemberS{Value: id},
+			"url":      &types.AttributeValueMemberS{Value: body.URL},
+			"selector": &types.AttributeValueMemberS{Value: body.Selector},
+			"regex":    &types.AttributeValueMemberS{Value: body.Regex},
+		},
+	})
+	if err != nil {
+		return "", fmt.Errorf("failed to put item: %w", err)
+	}
+
+	// Return the generated ID
+	return id, nil
 }
