ca7ebec3122263803f2c657957dbe9ac379856aa
richardkyk <28803078+richardkyk@users.noreply.github.com>
2025-08-18 21:04:54 +1000

    refactor: remove custom domain

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,15 +1,7 @@
 variable "app_name" {
-  type    = string
+  type = string
 }
 
 variable "aws_region" {
-  type    = string
-}
-
-variable "api_domain_name" {
-  type    = string
-}
-
-variable "api_mapping_key" {
-  type    = string
+  type = string
 }
